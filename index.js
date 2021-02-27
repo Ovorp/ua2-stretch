df23c358eef891c69aaf204ea95eb3fa08c1b4a3
Nathaniel Shirts <nshirts7@gmail.com>
2021-02-26 22:56:14 -0700

    added validateForm function and toggler

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 const formBtn = document.querySelector('#close-form');
 const form = document.querySelector('.form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const emailList = [];
+
+function validateForm() {
+  if (!nameInput.value && !emailInput.value) {
+    nameInput.style.border = 'solid red 2px';
+    emailInput.style.border = 'solid red 2px';
+    alert('You must enter a name and an email address to subscribe!');
+  } else if (!nameInput.value) {
+    nameInput.style.border = 'solid red 2px';
+    alert('Please enter a name to Subscribe!');
+  } else if (!emailInput.value) {
+    emailInput.style.border = 'solid red 2px';
+    alert('Please enter an email address to Subscribe!');
+  } else {
+    emailList.push({ name: nameInput.value, email: emailInput.value });
+  }
+}
+
+function toggler() {
+  form.classList.toggle('hide');
+  formBtn.innerText === 'X'
+    ? (formBtn.innerText = '+')
+    : (formBtn.innerText = 'X');
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  validateForm();
+});
+formBtn.addEventListener('click', toggler);
