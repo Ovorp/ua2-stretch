8e40199dd27ede71911883810dc6e8f042cbd49c
Nathaniel Shirts <nshirts7@gmail.com>
2021-02-27 16:32:55 -0700

    finished the stretch project

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ const form = document.querySelector('.form');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const emailList = [];
+const formContainer = document.querySelector('.form-container');
+const cart = document.createElement('div');
+const main = document.querySelector('main');
+let cartItems = 0;
+
+function addToCart() {
+  if (cartItems === 0) {
+    cartItems = 1;
+    cart.setAttribute('class', 'cart-display');
+    cart.innerText = `Your Cart: 1 item`;
+    main.appendChild(cart);
+  } else {
+    cartItems++;
+    cart.innerText = `Your Cart: ${cartItems} items`;
+  }
+}
+
+function displayThankYou() {
+  formContainer.innerText = 'Thank you for Subscribing!';
+  setTimeout(function () {
+    formContainer.remove();
+  }, 3000);
+}
 
 function validateForm() {
   if (!nameInput.value && !emailInput.value) {
@@ -16,6 +39,7 @@ function validateForm() {
     emailInput.style.border = 'solid red 2px';
     alert('Please enter an email address to Subscribe!');
   } else {
+    displayThankYou();
     emailList.push({ name: nameInput.value, email: emailInput.value });
   }
 }
@@ -27,8 +51,12 @@ function toggler() {
     : (formBtn.innerText = 'X');
 }
 
+nameInput.addEventListener('change', () => (nameInput.style.border = 'none'));
+emailInput.addEventListener('change', () => (emailInput.style.border = 'none'));
+
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   validateForm();
 });
+
 formBtn.addEventListener('click', toggler);
